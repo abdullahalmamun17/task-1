0740c98d3269330c1c5cb1c9f886a41d3b281013
abdullahalmamun17 <khondakarmamunfci@gmail.com>
2021-05-30 18:47:13 +0600

    done

--- src/data/data.js
+++ src/data/data.js
@@ -19,6 +19,36 @@ const data = [
         isDelete: false,
         isApprove: false
     },
+    {
+        id: 3,
+        moduleName: 'Purchase',
+        activityName: 'Purchase',
+        isCreate: false,
+        isView: false,
+        isEdit: false,
+        isDelete: false,
+        isApprove: false
+    },
+    {
+        id: 4,
+        moduleName: 'Sales',
+        activityName: 'Sales',
+        isCreate: false,
+        isView: false,
+        isEdit: false,
+        isDelete: false,
+        isApprove: false
+    },
+    {
+        id: 5,
+        moduleName: 'Accounts',
+        activityName: 'Voucher',
+        isCreate: false,
+        isView: false,
+        isEdit: false,
+        isDelete: false,
+        isApprove: false
+    },
 ]
 
 export default data;
